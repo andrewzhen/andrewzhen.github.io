6b7332ea6121440d275dad2bd816e2523e3c533c
Andrew Zhen <Andrew@Andrews-MacBook-Pro.local>
2017-07-08 16:04:46 -0700

    modals and smaller images

--- script.js
+++ script.js
@@ -9,17 +9,24 @@ ga('send', 'pageview');
 
 // start up
 window.onload = function() {
-	// Basketball tab
     var x = document.getElementById("btn1");
     refreshContent("Photography", "Basketball", "Traveling", "School");
-
-	// Photography tab
-	// var x = document.getElementById("btn2");
-	// refreshContent("School", "Photography", "Basketball", "Traveling");
 	
     x.style.fontSize = "36px";
     x.style.borderTopColor = "black";
 }
+
+// social media modal
+var weChatModal = document.getElementById('weChatModal'); var weChatBtn = document.getElementById("weChat");
+var snapChatModal = document.getElementById('snapChatModal'); var snapChatBtn = document.getElementById("snapChat");
+
+weChatBtn.onclick = function() { weChatModal.style.display = "block";}
+
+snapChatBtn.onclick = function() { snapChatModal.style.display = "block";}
+
+snapChatSpan.onclick = function() { snapChatModal.style.display = "none";}
+
+weChatSpan.onclick = function() { weChatModal.style.display = "none";}
 
 function focus1() {
     var x = document.getElementById("btn1");
@@ -105,33 +112,39 @@ function refreshContent(show, hide1, hide2, hide3) {
 	toHide3.style.display = "none";
 }
 
-// scroll to top
-window.onscroll = function() {
-	scrollFunction();
+// image gallery modal
+var pho1Modal = document.getElementById("pho1Modal"); var pho1Btn = document.getElementById("pho1");
+var pho2Modal = document.getElementById("pho2Modal"); var pho2Btn = document.getElementById("pho2");
+var pho3Modal = document.getElementById("pho3Modal"); var pho3Btn = document.getElementById("pho3");
+var pho4Modal = document.getElementById("pho4Modal"); var pho4Btn = document.getElementById("pho4");
+var pho5Modal = document.getElementById("pho5Modal"); var pho5Btn = document.getElementById("pho5");
+var phoM6odal = document.getElementById("pho6Modal"); var pho6Btn = document.getElementById("pho6");
+var pho7Modal = document.getElementById("pho7Modal"); var pho7Btn = document.getElementById("pho7");
+var pho8Modal = document.getElementById("pho8Modal"); var pho8Btn = document.getElementById("pho8");
+var pho9Modal = document.getElementById("pho9Modal"); var pho9Btn = document.getElementById("pho9");
+
+pho1Btn.onclick = function() { pho1Modal.style.display = "block";}
+pho2Btn.onclick = function() { pho2Modal.style.display = "block";}
+pho3Btn.onclick = function() { pho3Modal.style.display = "block";}
+pho4Btn.onclick = function() { pho4Modal.style.display = "block";}
+pho5Btn.onclick = function() { pho5Modal.style.display = "block";}
+pho6Btn.onclick = function() { pho6Modal.style.display = "block";}
+pho7Btn.onclick = function() { pho7Modal.style.display = "block";}
+pho8Btn.onclick = function() { pho8Modal.style.display = "block";}
+pho9Btn.onclick = function() { pho9Modal.style.display = "block";}
+
+window.onclick = function(event) {
+	if( event.target == weChatModal || event.target == snapChatModal ||
+		event.target == pho1Modal || event.target == pho2Modal ||
+		event.target == pho3Modal || event.target == pho4Modal ||
+		event.target == pho5Modal || event.target == pho6Modal ||
+		event.target == pho7Modal || event.target == pho8Modal || 
+		event.target == pho9Modal) {
+		weChatModal.style.display = "none"; snapChatModal.style.display = "none";
+		pho1Modal.style.display = "none"; pho2Modal.style.display = "none";
+		pho3Modal.style.display = "none"; pho4Modal.style.display = "none";
+		pho5Modal.style.display = "none"; pho6Modal.style.display = "none";
+		pho7Modal.style.display = "none"; pho8Modal.style.display = "none";
+		pho9Modal.style.display = "none";
+	}
 }
-
-function scrollFunction() {
-    if (document.body.scrollTop > 4000 || document.documentElement.scrollTop > 4000) {
-        document.getElementById("topButton").style.display = "block";
-    } else {
-        document.getElementById("topButton").style.display = "none";
-    }
-}
-
-// var modal = document.getElementById('myModal');
-// var btn = document.getElementById('wechat');
-// var span = document.getElementById("close")[0];
-
-// fa fa-wechat.onclick = function() {
-// 	modal.style.display = "block";
-// }
-
-// span.onclick = function() {
-// 	modal.style.dispaly = "none";
-// }
-
-// window.onclick = function(event) {
-// 	if(event.target == modal) {
-// 		modal.style.display = "none";
-// 	}
-// }
