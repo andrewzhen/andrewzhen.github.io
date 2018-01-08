c5bb00689086c0a1a9760d58922d81584b26274b
Andrew Zhen <ajzhen@ucsd.edu>
2018-01-07 18:34:31 -0800

    Update script.js

--- script.js
+++ script.js
@@ -22,8 +22,7 @@ window.onscroll = function() { scrollFunction(); }
 function scrollFunction() {
     if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
         document.getElementById("topBtn").style.display = "block";
-    }
-    else {
+    } else {
         document.getElementById("topBtn").style.display = "none";
     }
 }
@@ -33,12 +32,62 @@ function topFunction() {
 }
 
 //Social Media Modals
-var weChatModal = document.getElementById('weChatModal'); var weChatBtn = document.getElementById("wechat");
-var snapChatModal = document.getElementById('snapChatModal'); var snapChatBtn = document.getElementById("snapchat");
-weChatBtn.onclick = function() { weChatModal.style.display = "block";}
-weChatSpan.onclick = function() { weChatModal.style.display = "none";}
-snapChatBtn.onclick = function() { snapChatModal.style.display = "block";}
-snapChatSpan.onclick = function() { snapChatModal.style.display = "none";}
+var weChatModal = document.getElementById('weChatModal'); 
+var weChatBtn = document.getElementById("wechat");
+var snapChatModal = document.getElementById('snapChatModal'); 
+var snapChatBtn = document.getElementById("snapchat");
+weChatBtn.onclick = function() { 
+    weChatModal.style.display = "block";
+    document.getElementById("topBtn").style.display = "none";
+    disableScroll();
+}
+weChatModal.onclick = function() { 
+    weChatModal.style.display = "none";
+    document.getElementById("topBtn").style.display = "block";
+    enableScroll();
+}
+snapChatBtn.onclick = function() { 
+    snapChatModal.style.display = "block";
+    document.getElementById("topBtn").style.display = "none";
+    disableScroll();
+}
+snapChatModal.onclick = function() { 
+    snapChatModal.style.display = "none";
+    document.getElementById("topBtn").style.display = "block";
+    enableScroll();
+}
+
+//Disable Scrolling for Modals
+var keys = {37:1, 38:1, 39:1, 40:1};
+
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;
+}
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null; 
+    window.onwheel = null; 
+    window.ontouchmove = null;  
+    document.onkeydown = null;  
+}
 
 //Category Buttons
 function focus1() {
