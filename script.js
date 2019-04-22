a9759355911bc3b5f5a2c51fb1adc0d4981971e1
Andrew Zhen <ajzhen@ucsd.edu>
2019-04-22 00:22:06 -0700

    implement dark mode

---
+++ script.js
@@ -0,0 +1,64 @@
+var turnOn = false;
+localStorage.setItem("status", turnOn);
+var status = localStorage.getItem("turnOn");
+var bg = document.getElementById("light-bg").style;
+var loc = document.getElementById("home-location");
+
+if (turnOn) {
+  darkmode();
+  document.getElementById("checkDark").checked = true;
+}
+
+function darkmode() {
+  if (turnOn) {
+    try {
+      bg.filter = "grayscale(30%)";
+      bg.backgroundImage = "url(assets/light-bg.jpg)";
+      loc.innerHTML = "Mt Woodson Summit, San Diego";
+    } catch (error) {
+      console.log(error);
+    }
+    changeColor("black", "white");
+
+    turnOn = false;
+  } else {
+    try {
+      bg.filter = "grayscale(0%)";
+      bg.backgroundImage = "url(assets/dark-bg.jpg)";
+      loc.innerHTML = "Sunset Cliffs, San Diego";
+    } catch (error) {
+      console.log(error);
+    }
+    changeColor("white", "black");
+    
+    turnOn = true;
+  }
+}
+
+function changeColor(newColor, otherColor) {
+  document.getElementById("body").style.backgroundColor = otherColor;
+
+  const elementsList = document.querySelectorAll("#body, #home, #name, #about, #photo, #description, #description-2, #home-location, #email, #resume, #linkedin, #github, #instagram");
+  const elementsArray = [...elementsList];
+
+  elementsArray.forEach(element => {
+    element.style.color = newColor;
+  })
+}
+
+function mo(id) {
+  document.getElementById(id).style.color = "#9E9E9E";
+}
+
+function ml(id) {
+  if (document.getElementById("body").style.backgroundColor == "black") {
+    document.getElementById(id).style.color = "white";
+  } else {
+    document.getElementById(id).style.color = "black";
+  }
+  // if (turnOn) {
+  //   document.getElementById(id).style.color = "black";
+  // } else {
+  //   document.getElementById(id).style.color = "white";
+  // }
+}
